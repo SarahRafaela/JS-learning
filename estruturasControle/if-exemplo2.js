2cf118553cf2cd7980376f8e590e5fd35440613e
SarahRafaela <sarahrafaela3@gmail.com>
2020-10-20 09:54:08 -0300

    estruturas de controle

---
+++ estruturasControle/if-exemplo2.js
@@ -0,0 +1,20 @@
+function teste1(num) {
+    if(num > 7)
+        console.log(num)
+    
+    console.log('Final')
+}
+
+teste1(6)
+teste1(8)
+
+// cuidado com o ';', não usar com as estruturas de controle
+
+function teste2(num) {
+    if(num > 7); { 
+        console.log(num)
+    }
+}
+
+teste2(6)
+teste2(8)
---
+++ estruturasControle/ifElse.js
@@ -0,0 +1,12 @@
+const imprimirResultado = function(nota){
+    if (nota>=7){
+        console.log('Aprovado!')
+    } else{
+        console.log('Reprovado!')
+    }
+}
+
+imprimirResultado(10)
+
+//cuidado
+imprimirResultado("nota")
